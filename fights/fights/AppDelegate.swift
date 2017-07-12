441a53cc604beb1ff28aceca0a9b5fc520955250
klimenkoyuri <klimenko@styleru.net>
2017-07-12 14:23:30 +0300

    начать заново

--- fights/fights/AppDelegate.swift
+++ fights/fights/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  Fights
+//  fights
 //
 //  Created by Юрий on 12.07.17.
 //  Copyright © 2017 Styleru. All rights reserved.
@@ -53,7 +53,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "Fights")
+        let container = NSPersistentContainer(name: "fights")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
--- fights/fights/ViewController.swift
+++ fights/fights/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Fights
+//  fights
 //
 //  Created by Юрий on 12.07.17.
 //  Copyright © 2017 Styleru. All rights reserved.
